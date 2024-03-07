6f83be7f7f6af5b4602f92e43c5c17d2fc75a8b7
Taleem <taleemmankuer@gmail.com>
2024-03-07 11:21:43 +0300

    add home page to root route/path

--- app/page.js
+++ app/page.js
@@ -1,95 +1,109 @@
 "use client";
 import styles from "./page.module.scss";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Hero from "@/components/Hero/Hero";
+import Homepage from "@/components/Home/Home";
+import LatestTrailers from "@/components/LatestTrialers/LatestTrailers";
+import Iframe from "@/components/Iframe/Iframe";
 import Image from "next/image";
-import Link from "next/link";
-import { BsFillArrowRightCircleFill } from "react-icons/bs";
-import {
-  FacebookShareButton,
-  FacebookIcon,
-  FacebookMessengerShareButton,
-  FacebookMessengerIcon,
-  RedditShareButton,
-  RedditIcon,
-  TelegramShareButton,
-  TelegramIcon,
-  TwitterShareButton,
-  TwitterIcon,
-  WhatsappShareButton,
-  WhatsappIcon,
-} from "next-share";
 
-export default function Landing() {
-  return (
-    <div className={styles.main}>
-      <Link href={"/home"}>
-        <h1>
-          MovieHub2Day <div></div>
-        </h1>
-      </Link>
-      <h2 className={styles.h2}>
-        <strong>Watch Movies Online in HD for Free!</strong>
-      </h2>
-      <Link className={styles.form} href={"/search"}>
-        <form action="">
-          <input type="text" placeholder="search for a movie, tv show, person..." />
-          <button type="submit">search</button>
-        </form>
-      </Link>
-      <p>Please share MovieHub2Day with your friends and family.</p>
-      <div className={styles.share}>
-        <FacebookShareButton
-          url={`https://moviehub2day.vercel.app/home`}
-          title={"MovieHub2Day is a free online movie streaming and downloading platform"}
-        >
-          <FacebookIcon />
-        </FacebookShareButton>
-        <TwitterShareButton
-          url={`https://moviehub2day.vercel.app/home`}
-          title={"MovieHub2Day is a free online movie streaming and downloading platform"}
-        >
-          <TwitterIcon />
-        </TwitterShareButton>
-        <FacebookMessengerShareButton
-          url={`https://moviehub2day.vercel.app/home`}
-          title={"MovieHub2Day is a free online movie streaming and downloading platform."}
-        >
-          <FacebookMessengerIcon />
-        </FacebookMessengerShareButton>
-        <RedditShareButton
-          url={`https://moviehub2day.vercel.app/home`}
-          title={"MovieHub2Day is a free online movie streaming and downloading platform"}
-        >
-          <RedditIcon />
-        </RedditShareButton>
-        <WhatsappShareButton
-          url={`https://moviehub2day.vercel.app/home`}
-          title={"MovieHub2Day is a free online movie streaming and downloading platform"}
-        >
-          <WhatsappIcon />
-        </WhatsappShareButton>
-        <TelegramShareButton
-          url={`https://moviehub2day.vercel.app/home`}
-          title={"MovieHub2Day is a free online movie streaming and downloading platform"}
-        >
-          <TelegramIcon />
-        </TelegramShareButton>
-      </div>
-      <Link href={`/home`}>
-        <button className={styles.sitebtn}>
-          View Full Site <BsFillArrowRightCircleFill />
-        </button>
-      </Link>
-      <div className={styles.info}>
-        <p>Watch movies and series online for free</p>
-        <p>No account registration required</p>
-        <p>downloads available via torrent</p>
-        <p>very few ads</p>
-      </div>
-      <Link href={`/home`}>
-        <button className={styles.sitebtn}>
-          Go To MovieHub2Day <BsFillArrowRightCircleFill />
-        </button>
-      </Link>
+export default function Home() {
+  // created one state and added the most of the states into an object
+  const [allmovies, setAllmovies] = useState({
+    movies: [],
+    tv: [],
+    topmovies: [],
+    toptv: [],
+    randompage: Math.floor(Math.random() * 10 + 1),
+    loading: true,
+  });
+  // ....
+  const [movieid, setmovieid] = useState(0);
+  const [open, setopen] = useState(false);
+
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY}&page=1`
+    );
+
+    setAllmovies((setmovies) => {
+      return { ...setmovies, movies: data.results };
+    });
+
+    const data2 = await axios.get(
+      `https://api.themoviedb.org/3/trending/tv/day?api_key=${process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY}&page=1`
+    );
+
+    setAllmovies((settv) => {
+      return { ...settv, tv: data2.data.results };
+    });
+
+    const data3 = await axios.get(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY}&page=${allmovies.randompage}`
+    );
+
+    setAllmovies((settopmovies) => {
+      return { ...settopmovies, topmovies: data3.data.results };
+    });
+
+    const data4 = await axios.get(
+      `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY}&page=${allmovies.randompage}`
+    );
+
+    setAllmovies((settoptv) => {
+      return { ...settoptv, toptv: data4.data.results };
+    });
+
+    setAllmovies((setloading) => {
+      return { ...setloading, loading: false };
+    });
+  };
+
+  const handleopen = (m) => {
+    setmovieid(m.id);
+    setopen(true);
+  };
+
+  const handleclose = () => {
+    setopen(false);
+  };
+
+  useEffect(() => {
+    fetchTrending();
+  }, []);
+
+  return allmovies.loading ? (
+    <div className="loading">
+      <Image
+        src={"/loaderspinner.svg"}
+        alt="loading"
+        width={100}
+        height={100}
+      />
     </div>
+  ) : (
+    <main className={styles.main}>
+      {open && (
+        <Iframe
+          movieid={movieid}
+          handleclose={handleclose}
+          open={open}
+          type={"movie"}
+        />
+      )}
+      <Hero />
+      <Homepage
+        title={"Trending"}
+        movies={allmovies.movies}
+        tv={allmovies.tv}
+      />
+      <LatestTrailers handleopen={handleopen} />
+      <Homepage
+        title={"All time popular"}
+        movies={allmovies.topmovies}
+        tv={allmovies.toptv}
+      />
+    </main>
   );
 }
