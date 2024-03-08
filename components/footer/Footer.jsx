a07b5217fc0bb91ea579028044ae00ebceb79097
Taleem <taleemmankuer@gmail.com>
2024-03-08 13:58:27 +0300

    update footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -5,37 +5,24 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className={styles.container}>
-      <div className={styles.logo}>
-        <h1>
-          <strong>MovieHub2Day</strong>
-          <div></div>
-        </h1>
-        <p>
-          <strong>MovieHub2Day 2024</strong>
-        </p>
-      </div>
-      <div className={styles.footertxt}>
-        <p>
-          <strong>MovieHub2Day</strong> is a <strong>Free Movies streaming, downloading</strong> site and library with
-          fewer ads. We let you stream, download and/or explore{" "}
-          <strong>movies, tv series and people online free</strong>, with over 1,000,000 <strong>movies</strong>,{" "}
-          <strong>TV-Series</strong> and people for you to explore and stream or <strong>download</strong> and watch
-          later.
-        </p>
-        <ul>
-          <Link href={"/privacy-policy"}>
-            <li>Privacy Policy</li>
+      <div>
+        <div className={styles.logo}>
+          <h1>
+            <strong>MovieHub</strong>
+            <div></div>
+          </h1>
+        </div>
+        <p className={styles.footertxt}>
+          Made with by <AiOutlineHeart />{" "}
+          <Link href="https://taleemmankuer.vercel.app" target="_blank">
+            Taleem
           </Link>
-          <Link href={"mailto:contactmoviehub@proton.me"}>
-            <li>Contact</li>
+        </p>
+        <p className={styles.footertxt}>
+          Powered by{" "}
+          <Link href="https://developer.themoviedb.org/reference/intro/getting-started" target="_blanks">
+            TMDB's API
           </Link>
-        </ul>
-      </div>
-      <div className={styles.disclaimer}>
-        <span></span>
-        <p>
-          <strong>MovieHub2Day</strong> does not own any servers and therefore does not store any of the files on this
-          site. All data on this site is scrapped and linked off the internet from publicly available websites.
         </p>
       </div>
     </footer>
